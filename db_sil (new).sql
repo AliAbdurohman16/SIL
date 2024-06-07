0bdf0980f86cad836369aad76d9a07718ea95ad0
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-06-07 21:38:25 +0700

    tambahin sql terbaru

--- db_sil (new).sql
+++ db_sil (new).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 06, 2024 at 06:08 PM
+-- Generation Time: Jun 07, 2024 at 09:37 PM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -48,9 +48,10 @@ INSERT INTO `tbl_customers` (`id`, `no_rekam_medis`, `nik`, `nama`, `email`, `al
 (1, '0001/SIL/RM/03/2024', '3209021706010002', 'Mohammad Irwansyah Somantri', 'mirwansyah1933@gmail.com', 'Jl. RA Kartini, RT 04, RW 01, Blok Pon No. 45', 'Cirebon', '2023-11-30', 'Laki-laki', '0838252879888'),
 (3, '0002/SIL/RM/03/2024', '3209021706010003', 'admins', 'mirwansyahs989@gmail.com', 'Jl. RA Kartini, RT 04, RW 01, Blok Pon No. 45', 'Cirebon', '2024-11-28', 'Laki-laki', '083825287989'),
 (9, '0003/SIL/RM/03/2024', '3209021706010001', 'Mohammad Teguh Adriansyah', 'mta99910@gmail.com', 'Ciledug', 'Cirebon', '2023-12-27', 'Laki-laki', '0811231231231'),
-(10, '9237237', '3293237927323', 'sasa', 'asas@as.com', 'asas', 'sasa', '2024-06-04', 'asas', '23232'),
-(11, '32323', '3232', 'asas', 'asas@sa', 'sas', 'sasas', '2024-06-05', 'asa', '2323'),
-(13, '3232', '32326', '', '', '', '', '0000-00-00', '', '');
+(15, '32372979', '2323', 'asas', 'sasa@gmail.com', 'sdds', 'sdsd', '2001-03-01', 'Perempuan', '0876267367'),
+(16, '2323', '323', 'dwdsd', 'sdsds@gmail.com', 'dssd', 'dsdsd', '2012-01-31', 'Laki-laki', '23232323'),
+(18, '232323', '778394934', 'sasa', 'asasa@gmail.com', 'saasa', 'asas', '2001-05-06', 'Perempuan', '086863273'),
+(19, '9239372', '686673523', 'Mohammad Teguh Adriansyah', 'aasas@gmail.com', 'asasas', 'sasa', '2004-02-03', 'Perempuan', '0866657253');
 
 -- --------------------------------------------------------
 
@@ -65,16 +66,15 @@ CREATE TABLE `tbl_hasil_pemeriksaan` (
   `hasil` double DEFAULT '0',
   `tgl_pengujian` datetime DEFAULT NULL,
   `tgl_selesai` datetime DEFAULT NULL,
-  `dokter` varchar(255) DEFAULT NULL,
-  `penanggung_jawab` varchar(255) NOT NULL
+  `dokter` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_hasil_pemeriksaan`
 --
 
-INSERT INTO `tbl_hasil_pemeriksaan` (`hasil_id`, `kode_registrasi`, `parameter`, `hasil`, `tgl_pengujian`, `tgl_selesai`, `dokter`, `penanggung_jawab`) VALUES
-(6, '0001/REG/SIL/06/2024', 'Kolesterol', 2, '2024-06-04 00:00:00', '2024-06-04 00:00:00', 'dr. Nelly Ratnasari', 'dr. Yety Fauza, Sp.PK');
+INSERT INTO `tbl_hasil_pemeriksaan` (`hasil_id`, `kode_registrasi`, `parameter`, `hasil`, `tgl_pengujian`, `tgl_selesai`, `dokter`) VALUES
+(22, '0001/REG/SIL/06/2024', 'Kolesterol', 40, '2024-06-07 00:00:00', '2024-06-07 00:00:00', 'dr. Nelly Ratnasari');
 
 -- --------------------------------------------------------
 
@@ -122,6 +122,30 @@ INSERT INTO `tbl_parameter` (`id`, `kode`, `jenis_pemeriksaan`, `nama`, `satuan`
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_pembayaran`
+--
+
+CREATE TABLE `tbl_pembayaran` (
+  `invoice` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `kode_registrasi` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `jenis_pemeriksaan` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(255) NOT NULL DEFAULT 'Sudah Bayar',
+  `total` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_pembayaran`
+--
+
+INSERT INTO `tbl_pembayaran` (`invoice`, `kode_registrasi`, `nama`, `jenis_pemeriksaan`, `tanggal`, `status`, `total`) VALUES
+('INV6753', '0001/REG/SIL/06/2024', 'Mohammad Irwansyah Somantri', 'Hitung Darah Lengkap', '2024-06-07 14:50:26', 'Sudah Bayar', '3000'),
+('INV8229', '0001/REG/SIL/06/2024', 'Mohammad Teguh Adriansyah', 'Hitung Darah Lengkap', '2024-06-07 09:20:26', 'Sudah Bayar', '2000');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_pemeriksaan`
 --
 
@@ -141,7 +165,7 @@ CREATE TABLE `tbl_pemeriksaan` (
 --
 
 INSERT INTO `tbl_pemeriksaan` (`id`, `no_rekam_medis`, `kode_registrasi`, `jenis_pemeriksaan`, `catatan`, `isCito`, `status`, `tanggal`) VALUES
-(5, '9237237', '0001/REG/SIL/06/2024', 'Hitung Darah Lengkap', 'sdsd', 1, 'VERIF', '2024-06-04 10:34:40');
+(13, '9239372', '0001/REG/SIL/06/2024', 'Hitung Darah Lengkap', 'sasas', 1, 'VALID', '2024-06-07 09:20:26');
 
 -- --------------------------------------------------------
 
@@ -189,7 +213,7 @@ CREATE TABLE `tbl_user` (
 INSERT INTO `tbl_user` (`id_user`, `nama`, `photo`, `no_hp`, `username`, `password`, `role`) VALUES
 (6, 'admin', NULL, NULL, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin'),
 (9, 'ATLM', NULL, NULL, 'atlm', 'c1ccf3876d0ea9219293aecc341e6413', 'ATLM'),
-(10, 'Validator', NULL, NULL, 'validator', '8d6c391e7cb39133c91b73281a24f21f', 'Validator');
+(10, 'dr. Nelly Ratnasari', NULL, NULL, 'validator', '8d6c391e7cb39133c91b73281a24f21f', 'Validator');
 
 --
 -- Indexes for dumped tables
@@ -201,7 +225,8 @@ INSERT INTO `tbl_user` (`id_user`, `nama`, `photo`, `no_hp`, `username`, `passwo
 ALTER TABLE `tbl_customers`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nik` (`nik`),
-  ADD UNIQUE KEY `no_rekam_medis` (`no_rekam_medis`);
+  ADD UNIQUE KEY `no_rekam_medis` (`no_rekam_medis`),
+  ADD KEY `nama` (`nama`);
 
 --
 -- Indexes for table `tbl_hasil_pemeriksaan`
@@ -209,7 +234,8 @@ ALTER TABLE `tbl_customers`
 ALTER TABLE `tbl_hasil_pemeriksaan`
   ADD PRIMARY KEY (`hasil_id`),
   ADD KEY `kode_registrasi` (`kode_registrasi`),
-  ADD KEY `parameter` (`parameter`);
+  ADD KEY `parameter` (`parameter`),
+  ADD KEY `dokter` (`dokter`);
 
 --
 -- Indexes for table `tbl_jenis_pemeriksaan`
@@ -227,13 +253,23 @@ ALTER TABLE `tbl_parameter`
   ADD KEY `nama` (`nama`);
 
 --
+-- Indexes for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD PRIMARY KEY (`invoice`),
+  ADD KEY `kode_registrasi` (`kode_registrasi`),
+  ADD KEY `jenis_pemeriksaan` (`jenis_pemeriksaan`),
+  ADD KEY `nama` (`nama`);
+
+--
 -- Indexes for table `tbl_pemeriksaan`
 --
 ALTER TABLE `tbl_pemeriksaan`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `kode_registrasi` (`kode_registrasi`),
   ADD KEY `no_rekam_medis_2` (`no_rekam_medis`),
-  ADD KEY `status` (`status`);
+  ADD KEY `status` (`status`),
+  ADD KEY `jenis_pemeriksaan` (`jenis_pemeriksaan`);
 
 --
 -- Indexes for table `tbl_status`
@@ -246,7 +282,8 @@ ALTER TABLE `tbl_status`
 -- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `nama` (`nama`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -256,13 +293,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_customers`
 --
 ALTER TABLE `tbl_customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_hasil_pemeriksaan`
 --
 ALTER TABLE `tbl_hasil_pemeriksaan`
-  MODIFY `hasil_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `hasil_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `tbl_jenis_pemeriksaan`
@@ -280,7 +317,7 @@ ALTER TABLE `tbl_parameter`
 -- AUTO_INCREMENT for table `tbl_pemeriksaan`
 --
 ALTER TABLE `tbl_pemeriksaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_status`
@@ -302,8 +339,9 @@ ALTER TABLE `tbl_user`
 -- Constraints for table `tbl_hasil_pemeriksaan`
 --
 ALTER TABLE `tbl_hasil_pemeriksaan`
-  ADD CONSTRAINT `tbl_hasil_pemeriksaan_ibfk_1` FOREIGN KEY (`kode_registrasi`) REFERENCES `tbl_pemeriksaan` (`kode_registrasi`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbl_hasil_pemeriksaan_ibfk_2` FOREIGN KEY (`parameter`) REFERENCES `tbl_parameter` (`nama`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `tbl_hasil_pemeriksaan_ibfk_1` FOREIGN KEY (`kode_registrasi`) REFERENCES `tbl_pemeriksaan` (`kode_registrasi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_hasil_pemeriksaan_ibfk_2` FOREIGN KEY (`parameter`) REFERENCES `tbl_parameter` (`nama`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_hasil_pemeriksaan_ibfk_3` FOREIGN KEY (`dokter`) REFERENCES `tbl_user` (`nama`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_parameter`
@@ -312,11 +350,19 @@ ALTER TABLE `tbl_parameter`
   ADD CONSTRAINT `tbl_parameter_ibfk_1` FOREIGN KEY (`jenis_pemeriksaan`) REFERENCES `tbl_jenis_pemeriksaan` (`nama`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_1` FOREIGN KEY (`kode_registrasi`) REFERENCES `tbl_pemeriksaan` (`kode_registrasi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_2` FOREIGN KEY (`jenis_pemeriksaan`) REFERENCES `tbl_pemeriksaan` (`jenis_pemeriksaan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_pembayaran_ibfk_3` FOREIGN KEY (`nama`) REFERENCES `tbl_customers` (`nama`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `tbl_pemeriksaan`
 --
 ALTER TABLE `tbl_pemeriksaan`
-  ADD CONSTRAINT `tbl_pemeriksaan_ibfk_1` FOREIGN KEY (`status`) REFERENCES `tbl_status` (`code`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbl_pemeriksaan_ibfk_2` FOREIGN KEY (`no_rekam_medis`) REFERENCES `tbl_customers` (`no_rekam_medis`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `tbl_pemeriksaan_ibfk_1` FOREIGN KEY (`status`) REFERENCES `tbl_status` (`code`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_pemeriksaan_ibfk_2` FOREIGN KEY (`no_rekam_medis`) REFERENCES `tbl_customers` (`no_rekam_medis`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
